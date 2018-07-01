500adc672c3801406f2a3cfbdd978824def6844d
Raghav Vashisht <raghav2011rv@gmail.com>
2018-07-01 21:28:04 +0530

    Initial Commit

---
+++ animations2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  animations2
+//
+//  Created by Raghav Vashisht on 01/07/18.
+//  Copyright © 2018 Raghav Vashisht. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
