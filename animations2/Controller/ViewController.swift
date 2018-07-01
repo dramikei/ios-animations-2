a16170d42978f551e060f7125311793be673d76c
Raghav Vashisht <raghav2011rv@gmail.com>
2018-07-01 23:17:57 +0530

    Improved code

--- animations2/Controller/ViewController.swift
+++ animations2/Controller/ViewController.swift
@@ -27,29 +27,21 @@ class ViewController: UIViewController {
     
     @IBAction func menuBtnPressed(_ sender: Any) {
         if shuffleBtn.center == menuBtn.center {
-            shuffleBtn.isHidden = false
-            heartBtn.isHidden = false
-            repeatBtn.isHidden = false
+            setBtnHidden(false)
             UIView.animate(withDuration: 0.4) {
-                self.shuffleBtn.alpha = 1
-                self.heartBtn.alpha = 1
-                self.repeatBtn.alpha = 1
+                self.setBtnAlpha(1)
                 self.shuffleBtn.center = self.shuffleBtnCenter
                 self.heartBtn.center = self.heartBtnCenter
                 self.repeatBtn.center = self.repeatBtnCenter
             }
         } else {
             UIView.animate(withDuration: 0.4, animations: {
-                self.shuffleBtn.alpha = 0
-                self.heartBtn.alpha = 0
-                self.repeatBtn.alpha = 0
+                self.setBtnAlpha(0)
                 self.shuffleBtn.center = self.menuBtn.center
                 self.heartBtn.center = self.menuBtn.center
                 self.repeatBtn.center = self.menuBtn.center
             }) { (true) in
-                self.shuffleBtn.isHidden = true
-                self.heartBtn.isHidden = true
-                self.repeatBtn.isHidden = true
+                self.setBtnHidden(true)
             }
         }
     }
@@ -62,14 +54,18 @@ class ViewController: UIViewController {
         shuffleBtn.center = menuBtn.center
         heartBtn.center = menuBtn.center
         repeatBtn.center = menuBtn.center
-        
-        shuffleBtn.alpha = 0
-        heartBtn.alpha = 0
-        repeatBtn.alpha = 0
-        
-        shuffleBtn.isHidden = true
-        heartBtn.isHidden = true
-        repeatBtn.isHidden = true
+    }
+    
+    func setBtnHidden(_ bool: Bool) {
+        self.shuffleBtn.isHidden = bool
+        self.heartBtn.isHidden = bool
+        self.repeatBtn.isHidden = bool
+    }
+    
+    func setBtnAlpha(_ float: CGFloat) {
+        self.shuffleBtn.alpha = float
+        self.heartBtn.alpha = float
+        self.repeatBtn.alpha = float
     }
     
 }
